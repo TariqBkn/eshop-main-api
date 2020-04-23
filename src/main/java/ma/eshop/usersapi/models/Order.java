3375c095b83a2f1d36342089933d2a63ea0f9b4c
TariqBkn <boukouyen.tariq@gmail.com>
2020-04-23 01:46:56 +0000

    Edit Order Model

--- src/main/java/ma/eshop/usersapi/models/Order.java
+++ src/main/java/ma/eshop/usersapi/models/Order.java
@@ -1,15 +1,39 @@
 package ma.eshop.usersapi.models;
 
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
-
+@Entity
+@Table(name = "Order_table")
 public class Order {
-    @OneToMany
+    @Id
+    @GeneratedValue
+    private int id;
+    @OneToMany(mappedBy = "order")
     private List<OrderLine> orderLines;
 
-    float getTotalCost(){
+    public Order(){
+
+    }
+    public float getTotalCost(){
         return orderLines.stream()
                 .map(product->product.getCost())
                 .reduce(0f, (a, b) ->Float.sum(a,b));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<OrderLine> getOrderLines() {
+        return orderLines;
+    }
+
+    public void setOrderLines(List<OrderLine> orderLines) {
+        this.orderLines.clear();
+        this.orderLines.addAll(orderLines);
     }
 }
