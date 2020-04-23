41ee97073baacc657da43f9b246b7277fdd9d3ef
TariqBkn <boukouyen.tariq@gmail.com>
2020-04-23 02:40:50 +0000

    Create EntryPoint

---
+++ src/main/java/ma/eshop/usersapi/filters/EntryEntryPoint.java
@@ -0,0 +1,19 @@
+package ma.eshop.usersapi.filters;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EntryEntryPoint implements AuthenticationEntryPoint {
+
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
