385e56336d8cad90a64aa72e87fe010cdf419cac
TariqBkn <boukouyen.tariq@gmail.com>
2020-04-23 01:56:51 +0000

    Edit JWT Response Model

--- src/main/java/ma/eshop/usersapi/models/JwtResponse.java
+++ src/main/java/ma/eshop/usersapi/models/JwtResponse.java
@@ -5,8 +5,10 @@ import java.io.Serializable;
 public class JwtResponse implements Serializable {
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwtToken;
-    public JwtResponse(String jwtToken) {
+    private final User user;
+    public JwtResponse(String jwtToken, User user) {
         this.jwtToken = jwtToken;
+        this.user=user;
     }
     public String getToken() {
         return this.jwtToken;
