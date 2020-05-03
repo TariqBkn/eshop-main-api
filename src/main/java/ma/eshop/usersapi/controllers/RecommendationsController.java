b99308fd1b7bd14355c4c1f54e4d00b5dca65d2c
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-03 03:47:52 +0000

    Find similar products

--- src/main/java/ma/eshop/usersapi/controllers/RecommendationsController.java
+++ src/main/java/ma/eshop/usersapi/controllers/RecommendationsController.java
@@ -1,10 +1,30 @@
 package ma.eshop.usersapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ma.eshop.usersapi.models.Product;
+import ma.eshop.usersapi.services.ProductsService;
+import ma.eshop.usersapi.services.ProductsSimilarityService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/recommendations")
+@RequestMapping("/recommend")
 public class RecommendationsController {
+    @Inject
+    private ProductsService productsService;
+    @Inject
+    private ProductsSimilarityService productsSimilarityService;
 
+    //TODO: make unified response form
+    //TODO: remove null
+    @GetMapping("/products/{productId}")
+    public List<Product> findSimilarProductsById(@PathVariable int productId)  {
+        Optional<Product> product = productsService.findById(productId);
+        if(product.isPresent()){
+            return productsSimilarityService.getSimilarProductsOf(product.get());
+        }
+        return null;
+    }
 }
--- src/main/java/ma/eshop/usersapi/services/ProductsService.java
+++ src/main/java/ma/eshop/usersapi/services/ProductsService.java
@@ -1,7 +1,18 @@
 package ma.eshop.usersapi.services;
 
+import ma.eshop.usersapi.models.Product;
+import ma.eshop.usersapi.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.Optional;
 
 @Service
 public class ProductsService {
+    @Inject
+    private ProductRepository productRepository;
+
+    public Optional<Product> findById(int id){
+        return productRepository.findById(id);
+    }
 }
