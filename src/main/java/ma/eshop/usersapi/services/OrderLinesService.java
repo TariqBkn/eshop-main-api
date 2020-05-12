eb5fcd21916b6916789e0c82e810f0c76189ff91
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 05:35:00 +0000

    Create orderLinesService

---
+++ src/main/java/ma/eshop/usersapi/services/OrderLinesService.java
@@ -0,0 +1,39 @@
+package ma.eshop.usersapi.services;
+
+import ma.eshop.usersapi.models.OrderLine;
+import ma.eshop.usersapi.repositories.OrderLinesRepository;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Service
+public class OrderLinesService {
+
+    @Inject
+    private OrderLinesRepository orderLinesRepository;
+
+    public boolean existByProductId(int id){
+        return orderLinesRepository.existsByProductId(id);
+    }
+
+    public void updateQuantityByProductId(int productId, int quantity) {
+        orderLinesRepository.updateQuantityByProductId(productId, quantity);
+    }
+
+    public void save(OrderLine newOrderLine){
+        orderLinesRepository.save(newOrderLine);
+    }
+
+    public List<OrderLine> findAllUndoneOrderLinesOf(int userId) {
+        return orderLinesRepository.findAllNoneCheckedOrderLinesByOrderUserId(userId);
+    }
+
+    public void delete(int orderLineId) {
+        this.orderLinesRepository.deleteById(orderLineId);
+    }
+
+    public List<OrderLine> findOderLinesOfUser(int userId) {
+        return this.orderLinesRepository.findAllOrderLinesOfUserWithId(userId);
+    }
+}
