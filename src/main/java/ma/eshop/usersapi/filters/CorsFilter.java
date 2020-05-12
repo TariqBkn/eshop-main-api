749fe99d266fe476d277d371c96fd7d5aeb1fbb4
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 04:37:08 +0000

    Add support PATCH method in cors filter

--- src/main/java/ma/eshop/usersapi/filters/CorsFilter.java
+++ src/main/java/ma/eshop/usersapi/filters/CorsFilter.java
@@ -18,12 +18,12 @@ public class CorsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // HttpServletRequest request = (HttpServletRequest) req;
         // HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         // response.setHeader("Access-Control-Max-Age", "3600");
         // response.setHeader("Access-Control-Allow-Headers", " Content-Type, Accept, X-Requested-With, remember-me");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, Authorization, Content-Type, Accept, X-Requested-With, remember-me");
         filterChain.doFilter(request, response);
     }
 }
