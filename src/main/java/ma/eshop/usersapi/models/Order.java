1f6948557d8fb5c2577e96a595031c0669e56816
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 04:42:32 +0000

    Add necessary methods to Order.java

--- src/main/java/ma/eshop/usersapi/models/Order.java
+++ src/main/java/ma/eshop/usersapi/models/Order.java
@@ -1,19 +1,35 @@
 package ma.eshop.usersapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Entity
-@Table(name = "Order_table")
+@Table(name = "order_table")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Order {
     @Id
     @GeneratedValue
     private int id;
-    @OneToMany(mappedBy = "order")
-    private List<OrderLine> orderLines;
+    @OneToMany(cascade = CascadeType.ALL,
+            mappedBy = "order", orphanRemoval = true)
+     private List<OrderLine> orderLines;
+    @ManyToOne
+    private User user;
+
+    private boolean checkedOut=false;
 
     public Order(){
-
+        orderLines = new ArrayList<>();
     }
+
     public float getTotalCost(){
         return orderLines.stream()
                 .map(product->product.getCost())
@@ -35,5 +51,35 @@ public class Order {
     public void setOrderLines(List<OrderLine> orderLines) {
         this.orderLines.clear();
         this.orderLines.addAll(orderLines);
+    }
+    public boolean isCheckedOut(){
+        return checkedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        this.checkedOut = checkedOut;
+    }
+
+    public void checkoutOrder(){
+        checkedOut=true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void addOrderLine(OrderLine orderLine){
+        this.orderLines.add(orderLine);
+    }
+    public void removeOrderLine(int orderLineId){
+        orderLines = this.orderLines.stream().filter(orderLine -> orderLine.getId()!=orderLineId).collect(Collectors.toList());
+    }
+
+    public Optional<OrderLine> findOrderLineOfProduct(int productId) {
+        return orderLines.stream().filter(orderLine -> orderLine.getProductId()==productId).findFirst();
     }
 }
