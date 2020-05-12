a3088220e88844348444a7f70a3f9dbd42ecf111
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 04:39:58 +0000

    Add user data to JwtResponse.java

--- src/main/java/ma/eshop/usersapi/models/JwtResponse.java
+++ src/main/java/ma/eshop/usersapi/models/JwtResponse.java
@@ -5,12 +5,40 @@ import java.io.Serializable;
 public class JwtResponse implements Serializable {
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwtToken;
-    private final User user;
+    private int id;
+    private final String firstName;
+    private final String lastName;
+    private final boolean accountNonLocked;
+    private final String role;
     public JwtResponse(String jwtToken, User user) {
         this.jwtToken = jwtToken;
-        this.user=user;
+        this.id=id;
+        this.firstName=user.getFirstName();
+        this.lastName=user.getLastName();
+        this.accountNonLocked=user.isAccountNonLocked();
+        this.role=user.getRole().toString();
     }
     public String getToken() {
         return this.jwtToken;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
