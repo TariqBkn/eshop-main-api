2f4407ac04513c8ce9ccee1655c3507761aab43d
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 05:14:14 +0000

    Add findAllProducts(Pageable pageable)

--- src/main/java/ma/eshop/usersapi/repositories/ProductRepository.java
+++ src/main/java/ma/eshop/usersapi/repositories/ProductRepository.java
@@ -1,9 +1,15 @@
 package ma.eshop.usersapi.repositories;
 
 import ma.eshop.usersapi.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("select p from Product p")
+    Page<Product> findAllProducts(Pageable pageable);
 }
