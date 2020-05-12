1c9a3356a4a979a9a8b955017b36186e7a78aa20
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 04:45:07 +0000

    create Checkout(), getOrderById() and getOrderHistory() in OrdersController

--- src/main/java/ma/eshop/usersapi/controllers/OrdersController.java
+++ src/main/java/ma/eshop/usersapi/controllers/OrdersController.java
@@ -1,20 +1,39 @@
 package ma.eshop.usersapi.controllers;
 
 import ma.eshop.usersapi.models.Order;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import javax.transaction.Transactional;
+import ma.eshop.usersapi.models.User;
+import ma.eshop.usersapi.services.OrdersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/Orders")
+@RequestMapping("/orders")
 public class OrdersController {
 
-    @Transactional
-    void Checkout(Order order){
+    @Inject
+    private OrdersService ordersService;
 
+    @PatchMapping("/{orderId}/checkout")
+    ResponseEntity Checkout(@PathVariable int orderId, @AuthenticationPrincipal User user){
+        if(ordersService.userHasNoUndoneOrders(user.getId())) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No order found");
+        ordersService.checkoutOrder(orderId);
+        return ResponseEntity.ok().build();
     }
 
-    void getOrdersHistory(){
-
+    @GetMapping("/{orderId}")
+    ResponseEntity getOrderById(@PathVariable int orderId){
+        Optional<Order> order = ordersService.findOrderById(orderId);
+        if(order.isPresent()) return ResponseEntity.ok().body(order.get());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produit introuvable");
+    }
+    @GetMapping("/checkouts")
+    List<Order> getOrdersHistory(@AuthenticationPrincipal User connectedUser){
+        return ordersService.findCheckoutsOfConnectedUser(connectedUser.getId());
     }
 }
