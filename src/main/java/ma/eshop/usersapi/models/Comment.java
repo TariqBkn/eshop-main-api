917c65255e25e2ae537b193510d132264feeade3
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 05:33:28 +0000

    Create Comment model

---
+++ src/main/java/ma/eshop/usersapi/models/Comment.java
@@ -0,0 +1,35 @@
+package ma.eshop.usersapi.models;
+
+public class Comment {
+    private int id;
+    private String text;
+    private int rating;
+
+    public Comment(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
