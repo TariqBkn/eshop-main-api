e8560afdfcde20d1128c04dbd7ce16aab14b057b
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 05:15:34 +0000

    Add findById and getSimilarProductsOf methods to ProductsController

--- src/main/java/ma/eshop/usersapi/controllers/ProductsController.java
+++ src/main/java/ma/eshop/usersapi/controllers/ProductsController.java
@@ -2,15 +2,34 @@ package ma.eshop.usersapi.controllers;
 
 import ma.eshop.usersapi.models.Order;
 import ma.eshop.usersapi.models.Product;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ma.eshop.usersapi.models.SimilarProduct;
+import ma.eshop.usersapi.repositories.ProductRepository;
+import ma.eshop.usersapi.services.ProductsService;
+import ma.eshop.usersapi.services.SimilarProductsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/Carts")
+@RequestMapping("/products")
 public class ProductsController {
+
+    @Inject
+    private ProductsService productsService;
+
+    @Inject
+    private SimilarProductsService similarProductsService;
+
+
 
     void AddProductToWishList(Product product){
 
@@ -18,8 +37,26 @@ public class ProductsController {
     void removeProductFromWishList(){
 
     }
+    @RequestMapping("/{productId}")
+    ResponseEntity<Product> findById(@PathVariable int productId){
+        Optional<Product> product = productsService.findById(productId);
+        if(product.isPresent()){
+            return ResponseEntity.ok().body(product.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 
-//    void List<Product> getProductRecommendations(){
-//        return new ArrayList<Product>();
-//    }
+    @GetMapping("/{productId}/similar")
+    public ResponseEntity<List<SimilarProduct>> getSimilarProductsOf(@PathVariable int productId) throws UnsupportedEncodingException {
+        Optional<Product> product = productsService.findById(productId);
+        if(product.isPresent()){
+               return ResponseEntity.ok().body(similarProductsService.getSimilarProductsOf(productId));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("")
+    public Page<Product> getProducts(@RequestParam(defaultValue="0") int page){
+        return productsService.findAllProducts(PageRequest.of(page, 30));
+    }
 }
