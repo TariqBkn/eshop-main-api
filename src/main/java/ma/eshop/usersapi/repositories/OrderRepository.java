c1041dfeb2fa644d8b43d454001d53c3eb9fa906
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 04:43:36 +0000

    create OrderRepository.java

--- src/main/java/ma/eshop/usersapi/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.eshop.usersapi.repositories;
-
-import ma.eshop.usersapi.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository< Order, Integer> {
-}
