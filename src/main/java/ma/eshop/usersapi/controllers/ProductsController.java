172c2b4b143f368213174061e82b15b22f809ed5
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-13 06:10:41 +0000

    Create bulk add end point to ProductsController

--- src/main/java/ma/eshop/usersapi/controllers/ProductsController.java
+++ src/main/java/ma/eshop/usersapi/controllers/ProductsController.java
@@ -1,28 +1,23 @@
 package ma.eshop.usersapi.controllers;
 
-import ma.eshop.usersapi.models.Order;
 import ma.eshop.usersapi.models.Product;
 import ma.eshop.usersapi.models.SimilarProduct;
-import ma.eshop.usersapi.repositories.ProductRepository;
 import ma.eshop.usersapi.services.ProductsService;
 import ma.eshop.usersapi.services.SimilarProductsService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
-
     @Inject
     private ProductsService productsService;
 
@@ -30,13 +25,6 @@ public class ProductsController {
     private SimilarProductsService similarProductsService;
 
 
-
-    void AddProductToWishList(Product product){
-
-    }
-    void removeProductFromWishList(){
-
-    }
     @RequestMapping("/{productId}")
     ResponseEntity<Product> findById(@PathVariable int productId){
         Optional<Product> product = productsService.findById(productId);
@@ -59,4 +47,11 @@ public class ProductsController {
     public Page<Product> getProducts(@RequestParam(defaultValue="0") int page){
         return productsService.findAllProducts(PageRequest.of(page, 30));
     }
+
+    @GetMapping(value = "/bulk-add", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity bulkAddProducts() throws Exception{ //@RequestBody File productsFlatFile
+        return productsService.loadProductsFromFlatFileIntoDataBase();
+    }
+
+
 }
