8e443bca428ea7baf9c44481ef05de1f440c59fa
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-15 04:11:02 +0000

    Create UploadsService with uploadCsvFile and uploadImage methods

---
+++ src/main/java/ma/eshop/usersapi/services/UploadsService.java
@@ -0,0 +1,32 @@
+package ma.eshop.usersapi.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+@Service
+public class UploadsService {
+    @Value("${productsInputStorageLocation}")
+    String productsInputStorageLocation;
+
+    @Value("${imagesBaseLocation}")
+    String imagesBaseLocation;
+
+    public void uploadCsvFile(MultipartFile multipartProductsFile) throws IOException {
+        File productsCsvFile = new File(productsInputStorageLocation);
+        productsCsvFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(productsCsvFile);
+        fileOutputStream.write(multipartProductsFile.getBytes());
+        fileOutputStream.close();
+    }
+
+    public void uploadImage(MultipartFile multipartProductsFile,String name ) throws IOException {
+        File imageFile = new File(imagesBaseLocation+name);
+        imageFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
+        fileOutputStream.write(multipartProductsFile.getBytes());
+        fileOutputStream.close();
+    }
+}
