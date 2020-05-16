1501ed5bd071c4afd16a1e0f9bf41e9cbdd87d67
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-16 23:22:36 +0000

    Delete wishList functionality

--- src/main/java/ma/eshop/usersapi/controllers/WishListsController.java
+++
@@ -1,9 +0,0 @@
-package ma.eshop.usersapi.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/wishlists")
-public class WishListsController {
-}
