c69f9ac4c342d5917bc2d281d519aaba5123c3d5
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-16 11:43:07 +0000

    Add findByKeyword method in ProductRepository

--- src/main/java/ma/eshop/usersapi/repositories/ProductRepository.java
+++ src/main/java/ma/eshop/usersapi/repositories/ProductRepository.java
@@ -7,9 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("select p from Product p")
     Page<Product> findAllProducts(Pageable pageable);
+    @Query(   "select p from Product p where "
+            + "LOWER(p.title)  like CONCAT('%',?1,'%')"
+            + "or LOWER(p.color) like CONCAT('%',?1,'%')"
+            + "or LOWER(p.description) like CONCAT('%',?1,'%')"
+            + "or LOWER(p.providerName) like CONCAT('%',?1,'%')"
+             )
+    List<Product> findByKeyword(String keyword);
 }
