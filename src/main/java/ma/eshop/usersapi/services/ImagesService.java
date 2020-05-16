23b983121e996cae45e1fdf3ceeba98a5d3440cc
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-16 06:02:24 +0000

    Create deleteByName method in ImagesService

--- src/main/java/ma/eshop/usersapi/services/ImagesService.java
+++ src/main/java/ma/eshop/usersapi/services/ImagesService.java
@@ -9,30 +9,43 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
+
 @Service
 public class ImagesService {
     @Value("${imagesBaseLocation}")
     String imagesBaseLocation;
-    public ByteArrayResource findImageByName(String imageName) {
+    public ByteArrayResource findByName(String imageName) {
         String path=imagesBaseLocation+imageName;
         File file = new File(path);
         BufferedImage bImage = null;
         try {
-            System.out.println("FILE Path :"+path);
             bImage = ImageIO.read(file);
         } catch (IOException e) {
             System.out.println("INVALID_FILE:"+path);
         }
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         try {
-            ImageIO.write(bImage,"jpg", bos );
+            String formatName = imageName.split("\\.")[1];
+            ImageIO.write(bImage,formatName, bos );
         } catch (IOException e) {
-            e.printStackTrace();
+            try{
+                ImageIO.write(bImage,"png", bos );
+            }catch(IOException IOe){
+                IOe.printStackTrace();
+            }
         }
         byte [] data = bos.toByteArray();
         data =  Base64.getEncoder().encodeToString(data).getBytes();
         return new ByteArrayResource(data);
     }
+
+    public void deleteByName(String imageName) throws IOException {
+        Path imagePath = Paths.get(imagesBaseLocation+imageName);
+        Files.delete(imagePath);
     }
+}
 
